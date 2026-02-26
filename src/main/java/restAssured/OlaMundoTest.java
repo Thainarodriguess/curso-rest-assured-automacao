99092512caaecf6e76b0b30bedc221f40fbeba9c
Thaina Rodrigues <thainavieira660@gmail.com>
2026-02-26 19:08:49 -0300

    refactor(testes): implementar RequestSpecification e ResponseSpecification

--- src/main/java/restAssured/OlaMundoTest.java
+++ src/main/java/restAssured/OlaMundoTest.java
@@ -1,31 +1,40 @@
 package restAssured;
 
-import io.restassured.RestAssured;
-import io.restassured.http.Method;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 
 public class OlaMundoTest {
-    @Test
-    public void testOlaMundo(){
-        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
-        Assertions.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
-        Assertions.assertEquals(200, response.statusCode());
-    }
 
+    public static RequestSpecification  requisicaoPadrao;
+    public static ResponseSpecification respostaPadrao;
+
+    @BeforeAll
+    public static void setup(){
+        requisicaoPadrao = new RequestSpecBuilder()
+                .setBaseUri("https://restapi.wcaquino.me")
+                .build();
+
+        respostaPadrao = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+    
     @Test
     public void testDeveValidarBody(){
         given()
+             .spec(requisicaoPadrao)
        .when()
-             .get("https://restapi.wcaquino.me/ola")
+             .get("/ola")
        .then()
-            .statusCode(200)
+            .spec(respostaPadrao)
             .body(is("Ola Mundo!"))//Verifica se o corpo é EXATAMENTE igual a "Ola Mundo!" (case-sensitive)
             .body(containsString("Mundo"))//Verifica se o texto "Mundo" existe em QUALQUER parte do corpo da resposta
             .body(is(not(nullValue())))//Verifica se o valor NÃO é nulo (garante que a API retornou algum conteúdo)
@@ -35,9 +44,11 @@ public class OlaMundoTest {
     @Test
     public void deveVerificarPrimeiroNivelJson(){
         given()
+                .spec(requisicaoPadrao)
         .when()
-             .get("https://restapi.wcaquino.me/users/1")
+             .get("/users/1")
         .then()
+             .spec(respostaPadrao)
              .body("id", is(1))
              .body("name", containsString("da Silva"))
              .body("age", is(30))
@@ -47,10 +58,11 @@ public class OlaMundoTest {
     @Test
     public void deveVerificarSegundoNivelJson(){
         given()
+              .spec(requisicaoPadrao)
         .when()
-             .get("https://restapi.wcaquino.me/users/2")
+             .get("/users/2")
         .then()
-             .statusCode(200)
+             .spec(respostaPadrao)
              .body("name", containsString("Maria Joaquina"))//Verifica primeiro nível do Json
              .body("endereco.rua", is("Rua dos bobos"))//Verifica segundo nível do Json
         ;
@@ -59,10 +71,11 @@ public class OlaMundoTest {
     @Test
     public void deveVerificarLista(){
         given()
+                .spec(requisicaoPadrao)
         .when()
-                .get("https://restapi.wcaquino.me/users/3")
+                .get("/users/3")
         .then()
-                .statusCode(200)
+                .spec(respostaPadrao)
                 .body("name", containsString("Ana Júlia"))//Verifica primeiro nível do Json
                 .body("filhos", hasSize(2))//Verifica se a lista tem dois registros
                 .body("filhos[0].name", is("Zezinho"))//Faz a verificação dentro da lista filhos na posição 0
@@ -73,11 +86,11 @@ public class OlaMundoTest {
     @Test
     public void deveRetornarErroUsuarioInexistente(){
         given()
-                .when()
-                .get("https://restapi.wcaquino.me/users/4")
-                .then()
+                .spec(requisicaoPadrao)
+        .when()
+                .get("/users/4")
+        .then()
                 .statusCode(404)
-
                 .body("error", is("Usuário inexistente"))
         ;
     }
@@ -85,10 +98,11 @@ public class OlaMundoTest {
     @Test
     public void deveVerificarListaRaiz(){
         given()
-                .when()
-                .get("https://restapi.wcaquino.me/users")
-                .then()
-                .statusCode(200)
+                .spec(requisicaoPadrao)
+        .when()
+                .get("/users")
+        .then()
+                .spec(respostaPadrao)
                 .body("$", hasSize(3))//$ é uma convenção, não é obrigatório(pode deixar também vazio "") e faz a busca na raiz da lista
                 .body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia" ))//Busca atributo pelo nome
                 .body("age[1]", is(25))//Busca pela idade no index 1
@@ -101,10 +115,11 @@ public class OlaMundoTest {
     @Test
     public void deveFazerVerificacoesAvancadas(){
         given()
+                .spec(requisicaoPadrao)
         .when()
-                .get("https://restapi.wcaquino.me/users")
+                .get("/users")
         .then()
-                .statusCode(200)
+                .spec(respostaPadrao)
                 .body("$", hasSize(3))
                 .body("age.findAll{it <= 25}.size()", is(2))//Busca por idades menores ou igual a 25
                 .body("age.findAll{it <= 25 && it > 20}.size()", is(1))//Busca por usuários com mais de 20 anos e até 25
